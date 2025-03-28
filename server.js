8bfe28586f48556809805301ffbe10969f50d8bc
mohammedsarjun <sarjunsarjunms77@gmail.com>
2025-03-28 14:01:43 +0530

    Update file path

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the 'public' directory
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static('uploads'));
 
 //no cache
